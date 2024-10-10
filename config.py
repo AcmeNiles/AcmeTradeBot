d7b341d26ee9e2582d9b85775dee8959faaca36a
AcmeNiles <103822178+AcmeNiles@users.noreply.github.com>
2024-10-10 08:01:30 +0000

    moved to secrets

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
+import os
 # Define configuration constants
-URL = "https://b15a212e-ca66-4c21-ad34-76d56d3dc709-00-2jytsqhog1kqq.spock.replit.dev"
-ACME_API_KEY = "PRCAEUY-LA6UTNQ-XYL5DEI-ZNUEMDA"
-ADMIN_CHAT_ID = 1224919055
+URL = "https://acmeonetap.replit.app"
+ACME_API_KEY = os.getenv("ACME_API_KEY")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 PORT = 8000
-TOKEN = "7508232491:AAHZtt9-Jt1TlTfPpOCF__hewXj7LLui4mk"  # nosec B105
+TOKEN = URL = os.getenv("TOKEN")
 WELCOME_IMAGE_URL = "https://imagedelivery.net/P5lw0bNFpEj9CWud4zMJgQ/feecc12a-109f-417d-ed17-b5cee8fd1a00/public"
 ACME_URL = "https://acme-prod.fly.dev/operations/dev/"
