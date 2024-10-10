e508c9ce341bf2bb99b64108c82ffaa8e3f0bb14
AcmeNiles <103822178+AcmeNiles@users.noreply.github.com>
2024-10-10 08:05:55 +0000

    update secret

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ URL = "https://acmeonetap.replit.app"
 ACME_API_KEY = os.getenv("ACME_API_KEY")
 ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 PORT = 8000
-TOKEN = URL = os.getenv("TOKEN")
+TOKEN = URL = os.getenv("TELEGRAM_BOT_TOKEN")
 WELCOME_IMAGE_URL = "https://imagedelivery.net/P5lw0bNFpEj9CWud4zMJgQ/feecc12a-109f-417d-ed17-b5cee8fd1a00/public"
 ACME_URL = "https://acme-prod.fly.dev/operations/dev/"
