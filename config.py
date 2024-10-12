90ec3a343156bcaed296dd21e4c74258e73f29bc
AcmeNiles <103822178+AcmeNiles@users.noreply.github.com>
2024-10-12 13:21:37 +0000

    Prod vars

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
 ACME_URL = "https://acme-prod.fly.dev/operations/dev/"
 
 # Dev
-URL = "https://b15a212e-ca66-4c21-ad34-76d56d3dc709-00-2jytsqhog1kqq.spock.replit.dev"
-TOKEN = "6790358488:AAHg1Ml3Dqvco4IT7RfGjMaG4vCvPmSSUwA"
+#URL = "https://b15a212e-ca66-4c21-ad34-76d56d3dc709-00-2jytsqhog1kqq.spock.replit.dev"
+#TOKEN = "6790358488:AAHg1Ml3Dqvco4IT7RfGjMaG4vCvPmSSUwA"
+#ACME_GROUP = "@acmetest"
 
 # Production
-#URL = "https://acmeonetap.replit.app"
-#TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-#CHAT_GROUP = "@acmeonetap"  # Replace with your group's chat ID or username
+URL = "https://acmeonetap.replit.app"
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_GROUP = "@acmeonetap"  # Replace with your group's chat ID or username
 
 # Define conversation states
 SELECT_TOKEN, SELECT_AMOUNT, SELECT_RECIPIENT, WAITING_FOR_AUTH = range(4)
-ACME_GROUP = "@acmetest"
 
 # Global variables for valid and authenticated commands
 VALID_COMMANDS = {'trade', 'pay', 'request', 'token', 'share', 'vault', 'start','menu'}
